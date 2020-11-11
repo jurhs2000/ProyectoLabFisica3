7ffe22b6f0e73e3df7722de7e3010cbf42f04d62
Julio Herrera <jurhs2000@gmail.com>
2020-11-11 12:09:56 -0600

    Navegacion entre escenas

---
+++ Assets/Scripts/ParticleSelectSceneController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ParticleSelectSceneController : MonoBehaviour
+{
+    public void NextButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
